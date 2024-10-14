ad309aed5dca20c2a48d689a6bec1ed2c5de2c42
ariannacostanzo <costanzo.arianna@outlook.it>
2024-10-14 12:18:25 +0200

    adding jumbo

--- src/components/jumbotron/Jumbo.jsx
+++ src/components/jumbotron/Jumbo.jsx
@@ -1,8 +1,20 @@
+import './jumbo.scss'
+// da fare la logica 
+
 const Jumbo = () => {
      return (
-     <>
-       
-     </>
-     )
+       <>
+         <section id="jumbo">
+           <div className="writing-text container mx-auto my-10 flex justify-start gap-10">
+             <div>
+               Ciao, sono <span className="color-green">Arianna Costanzo</span> e
+               <h1>
+                 sono una <span className="color-purple">Web Developer</span>
+               </h1>
+             </div>
+           </div>
+         </section>
+       </>
+     );
 }
 export default Jumbo;
