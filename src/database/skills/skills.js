bc81c80223f06f92f89a2f25e7fca3157f5ecdd7
ariannacostanzo <costanzo.arianna@outlook.it>
2024-10-17 12:15:48 +0200

    fixed skills

--- src/database/skills/skills.js
+++ src/database/skills/skills.js
@@ -1,80 +1,72 @@
 const skills = [
-            {
-                'name' : 'html',
-                'image_path' : 'skills/html.png'
-            },
-            {
-                'name' : 'css',
-                'image_path' : 'skills/css.webp'
-            },
-            {
-                'name' : 'javascript',
-                'image_path' : 'skills/javascript.webp'
-            },
-            {
-                'name' : 'react',
-                'image_path' : 'skills/react.png'
-            },
-            {
-                'name' : 'node',
-                'image_path' : 'skills/node.png'
-            },
-            {
-                'name' : 'prisma',
-                'image_path' : 'skills/prisma.png'
-            },
-            {
-                'name' : 'java',
-                'image_path' : 'skills/java.png'
-            },
-            {
-                'name' : 'laravel',
-                'image_path' : 'skills/laravel.png'
-            },
-            {
-                'name' : 'inertia',
-                'image_path' : 'skills/inertia.svg'
-            },
-            {
-                'name' : 'sass',
-                'image_path' : 'skills/sass.png'
-            },
-            {
-                'name' : 'python',
-                'image_path' : 'skills/python.webp'
-            },
-            {
-                'name' : 'vite',
-                'image_path' : 'skills/vite.png'
-            },
-            {
-                'name' : 'vue',
-                'image_path' : 'skills/vue.png'
-            },
-            {
-                'name' : 'tailwind',
-                'image_path' : 'skills/tailwind.webp'
-            },
-            {
-                'name' : 'mysql',
-                'image_path' : 'skills/mysql.png'
-            },
-            {
-                'name' : 'c#',
-                'image_path' : 'skills/csharp.png'
-            },
-            {
-                'name' : 'git',
-                'image_path' : 'skills/git.png'
-            },
-            {
-                'name' : 'postman',
-                'image_path' : 'skills/postman.png'
-            },
-            {
-                'name' : 'unity',
-                'image_path' : 'skills/unity.png'
-            },
-]
+  {
+    name: "html",
+    image_path: "skills/html.png",
+  },
+  {
+    name: "css",
+    image_path: "skills/css.webp",
+  },
+  {
+    name: "javascript",
+    image_path: "skills/javascript.webp",
+  },
+  {
+    name: "react",
+    image_path: "skills/react.png",
+  },
+  {
+    name: "node",
+    image_path: "skills/node.png",
+  },
+  {
+    name: "prisma",
+    image_path: "skills/prisma.png",
+  },
+  {
+    name: "java",
+    image_path: "skills/java.png",
+  },
+  {
+    name: "laravel",
+    image_path: "skills/laravel.png",
+  },
+  {
+    name: "inertia",
+    image_path: "skills/inertia.svg",
+  },
+  {
+    name: "sass",
+    image_path: "skills/sass.png",
+  },
+  {
+    name: "python",
+    image_path: "skills/python.webp",
+  },
+  {
+    name: "vue",
+    image_path: "skills/vue.png",
+  },
+  {
+    name: "tailwind",
+    image_path: "skills/tailwind.webp",
+  },
+  {
+    name: "mysql",
+    image_path: "skills/mysql.png",
+  },
+  {
+    name: "c#",
+    image_path: "skills/csharp.png",
+  },
+  {
+    name: "git",
+    image_path: "skills/git.png",
+  },
+  {
+    name: "unity",
+    image_path: "skills/unity.png",
+  },
+];
 
 export default skills;
